4b058b153c52c6ffeb5241ad27a8b76bbbbcbb65
Andrew Giannico <40038319+Shyguy226@users.noreply.github.com>
2019-01-28 10:46:51 -0500

    Update funcs.cpp

--- funcs.cpp
+++ funcs.cpp
@@ -4,3 +4,19 @@
 
 
 // Add your functions here
+int fib (int num){
+  if(num < 3)
+    return 1;
+  else
+    return fib(num-1)+fib(num-2);
+}
+
+int sum (int a, int b){
+  return a + b;
+}
+
+int main(){
+  std::cout << fib(8) << " " << fib(2) << std::endl;
+  std::cout << sum(4,6) << " " << sum(2,3) << std::endl;
+  return 0;
+}
